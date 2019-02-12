8f42556350888459f049093d57a2c5f776d6eab4
Julian Martinsson <julianm@student.chalmers.se>
2019-02-12 11:15:53 +0100

    Added face class

--- handle_stl.py
+++ handle_stl.py
@@ -6,8 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+class Face:
+    def __init__(self, v1, v2, n):
+        self.v1 = v1
+        self.v2 = v2
+        self.n = n
+
 # Load model
-model = mesh.Mesh.from_file('models/u_shape_0.stl')
+model = mesh.Mesh.from_file('models/u_shape_45.stl')
 
 # Create new empty plot
 fig = plt.figure()
@@ -35,5 +41,23 @@ axes.scatter3D(model.x,model.y,model.z,color='red')
 for i in range(0,len(model.vectors)):
     axes.plot_wireframe(np.array(model.x[i]), np.array(model.y[i]), np.array([model.z[i], model.z[i]]), color="black")
 
+# Get faces
+faces = []
+for r in range(0,12):
+    v1 = np.array(model.vectors[r][0]) - np.array(model.vectors[r][1])
+    v2 = np.array(model.vectors[r][2]) - np.array(model.vectors[r][1])
+    n = model.normals[r]
+    res1 = np.dot(v1, n)
+    res2 = np.dot(v2, n)
+
+    # Ensure that the vectors are perpendicular to the normal
+    if (res1 != 0 or res2 != 0):
+        exit(1)
+    
+    f = Face(v1, v2, n)
+    faces.append(f)
+
 # Display plot
 plt.show()
+
+model.get
