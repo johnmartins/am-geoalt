4829b40d53bce77ecf3c22189e075f098d64e7d0
Julian Martinsson <julianm@student.chalmers.se>
2019-02-20 12:37:04 +0100

    edits to voxelizer

--- stl_2_voxel.py
+++ stl_2_voxel.py
@@ -6,13 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Convert to binary (TODO: if ascii)
-model = mesh.Mesh.from_file('models/cube.stl')
+model = mesh.Mesh.from_file('models/u_shape_arc.stl')
 model.get_mass_properties
 model.save('parse_this.stl', mode=stl.Mode.BINARY)
 
 input_path = 'parse_this.stl'
 
-resolution = 10
+resolution = 20
 model_mesh, bounding_box = read_and_reshape_stl(input_path, resolution)
 vxls, bounding_box = voxelize(model_mesh, bounding_box)
 
